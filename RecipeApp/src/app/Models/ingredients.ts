3d7272b7087164f5a9b5a686c09a6506adb5ac57
John Thompson <john.b.thompson.1985@gmail.com>
2023-09-25 14:14:19 -0400

    Changed Ingredients.tx
    Ingredients model needed to have 1 extra field to make it easier to work with when setting up ingredients.

--- RecipeApp/src/app/Models/ingredients.ts
+++ RecipeApp/src/app/Models/ingredients.ts
@@ -3,13 +3,33 @@ import { MeasurementType } from "./mesaruementType";
 export class Ingredient {
     public Name: string;
     public Measurement: string;
-    public Amount: string;
+    public WholeAmount: string;
+    public FractionAmount: string;
 
-    constructor(name: string, measurement: string, amount: string)
+    constructor(name: string, measurement: string, wholeAmount: string, 
+                    fractionAmount: string)
     {
         this.Name = name;
         this.Measurement = measurement;
-        this.Amount = amount;
+        this.WholeAmount = wholeAmount;
+        this.FractionAmount = fractionAmount;
+    }
+
+    getAmount(): string
+    {
+        let amount: string = '';
+
+        if (this.WholeAmount != '0')
+        {
+            amount+= this.WholeAmount + ' ';
+        }
+
+        if (this.FractionAmount != '0')
+        {
+            amount+= this.FractionAmount + ' ';
+        }
+
+        return amount;
     }
 
 }
