fd0feac54bb2137bcce3f74c87b339533e87a32f
John Thompson <john.b.thompson.1985@gmail.com>
2023-10-04 15:01:03 -0400

    Added new Tags array to Recipe model.

--- RecipeApp/src/app/Models/recipe.ts
+++ RecipeApp/src/app/Models/recipe.ts
@@ -5,13 +5,15 @@ export class Recipe {
     public Description: string;
     public Ingredients: Ingredient[];
     public Instructions: string[];
+    public Tags: string[];
 
-    constructor(name: string, description: string, ingredients: Ingredient[], instructions: string[])
+    constructor(name: string, description: string, ingredients: Ingredient[], instructions: string[], tags: string[])
     {
         this.Name = name;
         this.Description = description;
         this.Ingredients = ingredients;
         this.Instructions = instructions;
+        this.Tags = tags;
     }
 
 }
