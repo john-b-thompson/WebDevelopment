7f23629fce10236dfaded6959e5b40d03dfd8edf
John Thompson <john.b.thompson.1985@gmail.com>
2023-11-13 23:54:12 -0500

    Changed the layout of the recipe-list.
    The layout of recipe-list was changed significantly along with extra typescript added to account for the changes.

--- RecipeApp/src/app/Features/RecipeBook/recipe-list/recipe-list.component.ts
+++ RecipeApp/src/app/Features/RecipeBook/recipe-list/recipe-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Ingredient } from 'src/app/Models/ingredients';
 import { Recipe } from 'src/app/Models/recipe';
 import { State } from 'src/app/Models/recipeState';
+import { Tags } from 'src/app/Models/tags';
 import { RecipeService } from 'src/app/Services/recipe.service';
 import { selectRecipeList } from 'src/app/Store/Recipe/recipe.selectors';
 
@@ -18,8 +20,12 @@ export class RecipeListComponent implements OnInit {
   modalRecipe: Recipe = {} as Recipe;
   currentRecipeId: number = 0;
 
-
   recipeList: Recipe[] = [];
+  unAlteredRecipeList: Recipe[] = [];
+  filterTags: string[] = [];
+  selectedFilterTags: string[] = [];
+
+  searchForm: FormGroup = new FormGroup({});
 
   fakeIngreds: Ingredient[] = [
     new Ingredient('Beef', 'Pound', '0', '1/2'),
@@ -33,7 +39,7 @@ export class RecipeListComponent implements OnInit {
     'Place cheese on and serve.'
   ];
 
-  fakeTags: string[] = [];
+  fakeTags: string[] = ['Keto', 'Gluten Free', 'Sugar Free'];
   
   fakeRecipeList: Recipe[] = [
     new Recipe('CheeseBurger', 'Tasty thick piece of meat with cheese and a bun.',
@@ -44,12 +50,24 @@ export class RecipeListComponent implements OnInit {
               private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.recipeList = this.recipeService.GetRecipeList();
-    
+    this.unAlteredRecipeList = this.recipeService.getRecipeList();
+    this.recipeList = this.unAlteredRecipeList;
+    this.filterTags = new Tags().allTags();
+
+    this.createForms();
+
+    // remove this when complete
     if (this.recipeList.length == 0 )
     {
-      this.recipeList = this.fakeRecipeList;
+      for(let i = 0; i < 100; i++)
+      {
+        this.unAlteredRecipeList.push(new Recipe('CheeseBurger', 'Tasty thick piece of meat with cheese and a bun.',
+        this.fakeIngreds, this.fakeInstructions, this.fakeTags));
+      }
     }
+
+    
+    this.recipeList = this.unAlteredRecipeList;
   }
 
   onLoad(recipeId: number)
@@ -61,6 +79,46 @@ export class RecipeListComponent implements OnInit {
   onEdit()
   {
     this.router.navigate(['recipe/edit', this.currentRecipeId]);
+  }
+
+  // Will search the list of recipes names to find matching values
+  onSearch()
+  {
+    let lowerCaseQuery : string = this.searchForm.get('Search')?.value.toLowerCase(); 
+    this.recipeList = this.unAlteredRecipeList.filter((recipe) => recipe.Name.toLowerCase().includes(lowerCaseQuery));
+  }
+
+  onFilterChange(event: any)
+  {    
+    let filterTag = event.target.value;
+
+    if(event.target.checked)
+    {
+      this.selectedFilterTags.push(filterTag)
+      this.recipeList = this.recipeList.filter((recipe) => recipe.Tags.includes(filterTag));
+    }
+    else
+    {
+      if (this.selectedFilterTags.includes(filterTag))
+      {
+        let index = this.selectedFilterTags.indexOf(filterTag);
+        this.selectedFilterTags = this.selectedFilterTags.slice(index, 1); 
+      }
+    }
+  }
+
+  private createForms()
+  {    
+    this.searchForm = new FormGroup({
+      'Search': new FormControl<string>('', Validators.pattern(/^[a-z0-9_-]{8,15}$/))
+    });
+  }
+
+  // TODO: Need to creat this
+  // Will seperate the list and create pagination.
+  seperateRecipeList(int: number)
+  {
+
   }
 
 }
